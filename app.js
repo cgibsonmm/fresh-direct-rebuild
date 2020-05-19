dc1517fd7577b1f4995cd4214e9637de764a546a
corey gibson <cgibsonmm@gmail.com>
2020-05-19 11:15:45 -0400

    added test route

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const logger = require('morgan')
-const routes = require('./routes')
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const routes = require("./routes");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(logger('dev'))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(logger("dev"));
 
-app.use('/api', routes)
+app.get("/", (req, res) => res.send("hello"));
+app.use("/api", routes);
 
-module.exports = app
+module.exports = app;
