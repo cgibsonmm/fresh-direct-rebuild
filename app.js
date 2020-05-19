cb04e3438592dd1542e4684766b853850e621272
corey gibson <cgibsonmm@gmail.com>
2020-05-19 11:42:54 -0400

    testing removing data base

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const logger = require("morgan");
 const routes = require("./routes");
 
 const app = express();
+app.use(logger("dev"));
+app.get("/", (req, res) => res.send("hello"));
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(logger("dev"));
 
-app.get("/", (req, res) => res.send("hello"));
 app.use("/api", routes);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const db = require("./db");
 
 const PORT = process.env.PORT | 3000;
 
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
